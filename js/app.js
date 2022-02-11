3060cebc126ecbb74c97e13f1f2e774807ef7be4
Flávio Sales <flaviohdsa@gmail.com>
2022-02-11 13:12:05 +0800

    1102

--- js/app.js
+++ js/app.js
@@ -13,28 +13,35 @@
  * 
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-//Navigation
+// Global Variables
 
 const navBarList = document.querySelector('#navbar__list');
 const sections = document.querySelectorAll('section');
 
+
+// Global Functions
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Add navigation items 
+
 for(section of sections) {
     let navElement = document.createElement("li");
-    navElement.innerHTML = `<a class = 'menu__link' href ='#${section.getAttribute("id")}'>${section.getAttribute("data-nav")}</a>`;
+    navElement.innerHTML = `<a class = 'menu__link ${section.getAttribute("id")}' href ='#${section.getAttribute("id")}'>${section.getAttribute("data-nav")}</a>`;
     navBarList.appendChild(navElement);
 };
 
-document.querySelector('#navbar__list').addEventListener('click', function (e) {
+// Navigation Scroll
+
+navBarList.addEventListener('click', function (e) {
     e.preventDefault();
 
     if (e.target.classList.contains('menu__link')) {
@@ -43,18 +50,21 @@ document.querySelector('#navbar__list').addEventListener('click', function (e) {
     }
 });
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// Add sections as active//
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+document.addEventListener("scroll", function() {
+    for(section of sections) {
+        if (isInViewport(section)) {
+            const id = section.getAttribute("id");   
+            document.querySelector(`.${id}`).classList.add("active");
+            section.classList.add('your-active-class');
+        } else {
+                const id = section.getAttribute("id");  
+                document.querySelector(`.${id}`).classList.remove("active");
+              section.classList.remove("your-active-class");
+            }
+    }
+})
 
 // build the nav
 
@@ -76,5 +86,3 @@ document.querySelector('#navbar__list').addEventListener('click', function (e) {
 // Scroll to section on link click
 
 // Set sections as active
-
-
